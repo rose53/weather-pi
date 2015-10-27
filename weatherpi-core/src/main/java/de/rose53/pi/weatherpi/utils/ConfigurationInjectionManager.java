ac6b3f18c0839b5bb5d24d0e5a469b2850d7eb1f
Michael Rosenauer <michael.rosenauer@agfa.com>
2015-10-27 16:40:22 +0100

    added StringList configuration

--- weatherpi-core/src/main/java/de/rose53/pi/weatherpi/utils/ConfigurationInjectionManager.java
+++ weatherpi-core/src/main/java/de/rose53/pi/weatherpi/utils/ConfigurationInjectionManager.java
@@ -3,28 +3,37 @@ package de.rose53.pi.weatherpi.utils;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.List;
 import java.util.MissingResourceException;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
 
 public class ConfigurationInjectionManager {
 
     static final String INVALID_KEY = "Invalid key '{0}'";
     static final String MANDATORY_PARAM_MISSING = "No definition found for a mandatory configuration parameter : '{0}'";
+
     private final Properties properties = new Properties();
+
+    @Inject
+    Logger logger;
 
     @PostConstruct
     public void init() {
 
-        try (FileInputStream file = new FileInputStream("./configuration.properties");) {
+        try (FileInputStream file = new FileInputStream("./configuration.properties")) {
             //load all the properties from this file
             properties.load(file);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            logger.error("init:",e);
         }
     }
 
@@ -101,5 +110,17 @@ public class ConfigurationInjectionManager {
             }
             return param.defaultValue();
         }
+    }
+
+    @Produces
+    @StringListConfiguration
+    public List<String> stringListConfiguration(InjectionPoint ip) throws IllegalStateException {
+        StringListConfiguration param = ip.getAnnotated().getAnnotation(StringListConfiguration.class);
+        if (param.key() == null || param.key().length() == 0) {
+            return Collections.emptyList();
+        }
+        return properties.entrySet().stream().filter(e -> e.getKey().toString().startsWith(param.key()))
+                                             .map(e -> e.getValue().toString())
+                                             .collect(Collectors.toList());
     }
 }
