07d1a991482717e8782a4574220f045c224f6c7c
rose53 <michael.rosenauer@googlemail.com>
2015-12-21 21:04:32 +0100

    DB connection can now be configured

--- weatherpi-core/src/main/java/de/rose53/pi/weatherpi/database/DBConnectionExposer.java
+++ weatherpi-core/src/main/java/de/rose53/pi/weatherpi/database/DBConnectionExposer.java
@@ -6,31 +6,43 @@ import java.sql.SQLException;
 
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import de.rose53.pi.weatherpi.utils.StringConfiguration;
 
 public class DBConnectionExposer {
 
-	@Produces
-	public Connection expose() {
+    @Inject
+    Logger logger;
 
-		Connection connection = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
+    @Inject
+    @StringConfiguration(key="db.connection",defaultValue="jdbc:mysql://localhost/weatherpi?user=weatherpi&password=weatherpi")
+    String connectionUrl;
 
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/weatherpi?user=weatherpi&password=weatherpi");
+    @Produces
+    public Connection expose() {
 
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
-		return connection;
-	}
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-	public void closeConnection(@Disposes Connection connection) {
+            connection = DriverManager.getConnection(connectionUrl);
 
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            logger.error("expose: ",e);
+        }
+        return connection;
+    }
 
-	}
+    public void closeConnection(@Disposes Connection connection) {
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            logger.error("closeConnection: ",e);
+        }
+
+    }
 }
